db175e4fbf161c0b5fd55e51d38ce22cee2c4683
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2023-02-03 13:34:14 +0100

    Added database fetch

--- Backend/Infrastructure/ItemRepository.cs
+++ Backend/Infrastructure/ItemRepository.cs
@@ -1,12 +1,20 @@
 ﻿using Domain;
 using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
 public class ItemRepository : IItemRepository
 {
+    private readonly DatabaseContext _dbContext;
+    
+    public ItemRepository(DatabaseContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new NullReferenceException("DatabaseContext can not be null.");
+    }
+
     public List<Item> GetAllItems()
     {
-        throw new NotImplementedException();
+        return _dbContext.ItemTable.ToList();
     }
 }
