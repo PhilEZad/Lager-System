2a5d3dc59601cde40e05dde62bf4580fe214ebb3
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2023-02-03 13:35:42 +0100

    Added primary key, auto key generation

--- Backend/Infrastructure/DatabaseContext.cs
+++ Backend/Infrastructure/DatabaseContext.cs
@@ -9,6 +9,17 @@ public class DatabaseContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //Setting Primary Keys
+        modelBuilder.Entity<Item>()
+            .HasKey(x => x.Id)
+            .HasName("PK_Item");
+        
+        //Generate Keys
+        modelBuilder.Entity<Item>()
+            .Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+        
+        //Required Properties
     }
     
     public DbSet<Item> ItemTable { get; set; }
