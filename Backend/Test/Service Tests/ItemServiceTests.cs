c1732d6d8d1580c00e4523fa83a6f88b8c556033
Kenneth Hjort <kenn5157@easv365.dk>
2023-03-01 15:04:53 +0100

    EditItem test implementations done

--- Backend/Test/Service Tests/ItemServiceTests.cs
+++ Backend/Test/Service Tests/ItemServiceTests.cs
@@ -1,4 +1,5 @@
 using Application;
+using Application.DTO;
 using Application.Interfaces;
 using Domain;
 using FluentAssertions;
@@ -14,9 +15,9 @@ public class ItemServiceTests
     {
         Action test = () => new ItemService(null);
 
-        test.Should().Throw<NullReferenceException>().WithMessage("ItemRepository is null.");
+        test.Should().Throw<NullReferenceException>();
     }
-    
+
     [Fact]
     public void GetAllItems_WithReturnOfNull_ShouldThrowNullReferenceExceptionWithMessage()
     {
@@ -24,10 +25,10 @@ public class ItemServiceTests
         ItemService itemService = new ItemService(itemRepository.Object);
 
         itemRepository.Setup(x => x.GetAllItems()).Returns((List<Item>)null);
-        
+
         Action test = () => itemService.GetAllItems();
 
-        test.Should().Throw<NullReferenceException>().WithMessage("Unable to fetch items from database.");
+        test.Should().Throw<NullReferenceException>();
     }
 
     [Fact]
@@ -38,7 +39,76 @@ public class ItemServiceTests
 
         itemRepository.Setup(x => x.AddItem(""));
 
-        Action test = () => itemService.AddItem("");
-        test.Should().Throw<ArgumentException>().WithMessage("Name must not be empty");
+        Action test = () => itemService.AddItem(new AddItemRequest(""));
+        test.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void EditItem_WithUnnaturalId_ShouldReturnArgumentException()
+    {
+        var editItem = new EditItemRequest
+        {
+            Id = -1,
+            Name = "Name"
+        };
+
+        var itemRepository = new Mock<IItemRepository>();
+        ItemService itemService = new ItemService(itemRepository.Object);
+
+        itemRepository.Setup(x => x.EditItem(editItem));
+
+        // Hvid ID er ugyldigt, retuner ArgumentException
+        Action test = () => itemService.EditItem(editItem);
+        test.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void EditItem_WithEmptyName_ShouldReturnArgumentException()
+    {
+        var editItem = new EditItemRequest
+        {
+            Id = 1,
+            Name = ""
+        };
+
+        var itemRepository = new Mock<IItemRepository>();
+        ItemService itemService = new ItemService(itemRepository.Object);
+
+        itemRepository.Setup(x => x.EditItem(editItem));
+
+        // Hvis item Name ikke er tomt, retunere ArgumentException with message
+        Action test = () => itemService.EditItem(editItem);
+        test.Should().Throw<ArgumentException>();
+    }
+
+    // Hvis item ikke eksistere i DB, nullReferenceException
+    [Fact]
+    public void EditItem_NotExistInDB_ShouldReturnNullReferenceException()
+    {
+        var item = new Item
+        {
+            Id = 1,
+            Name = "Unedited"
+        };
+        var editItem = new EditItemRequest
+        {
+            Id = 2,
+            Name = "Edited"
+        };
+
+        var itemRepository = new Mock<IItemRepository>();
+        ItemService itemService = new ItemService(itemRepository.Object);
+
+        itemRepository.Setup(x => x.EditItem(editItem)).Returns(() =>
+        {
+            if (item.Id != editItem.Id){
+                return null;
+            }
+            item.Name = editItem.Name;
+            return item;
+        });
+
+        Action test = () => itemService.EditItem(editItem);
+        test.Should().Throw<NullReferenceException>();
     }
 }
