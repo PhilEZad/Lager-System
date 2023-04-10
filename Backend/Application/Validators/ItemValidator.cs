9486e1658ff27efdf7fc9b90de2c6eb5c5dcf3e1
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2023-04-10 19:58:26 +0200

    Added item validators

--- Backend/Application/Validators/ItemValidator.cs
+++ Backend/Application/Validators/ItemValidator.cs
@@ -7,5 +7,12 @@ public class ItemValidator : AbstractValidator<Item>
 {
     public ItemValidator()
     {
+        RuleFor(x => x.Id).GreaterThan(0).WithMessage("Id must be greater than 0.");
+        RuleFor(x => x.Id).NotNull().WithMessage("Id cannot be null.");
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Id cannot be empty.");
+        
+        RuleFor(x => x.Name).NotNull().WithMessage("Name cannot be null.");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name cannot be empty.");
+        RuleFor(x => x.Name).Matches("^[a-zA-Z0-9]*$").WithMessage("Item name must only contain alphanumeric characters, and can not contain spaces.");
     }
 }
